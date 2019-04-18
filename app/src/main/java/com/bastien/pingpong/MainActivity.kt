2c9891fd5041c20a680d8df94d2dacda9c52f3ec
Bastien Roy-Mazoyer <bastien@edgegap.com>
2019-04-18 01:21:36 -0400

    v1

---
+++ app/src/main/java/com/bastien/pingpong/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.bastien.pingpong
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.R
+import android.R.attr.button
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    var button = findViewById<View>(R.id.) as Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
